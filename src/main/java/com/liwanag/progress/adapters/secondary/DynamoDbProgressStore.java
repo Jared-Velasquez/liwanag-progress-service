c532b669e829dcc0f48966d2cc293adb57ab35a4
Jared-Velasquez <107715384+Jared-Velasquez@users.noreply.github.com>
2025-09-27 23:46:16 -0700

    feat: implement loading of unit, episode, and activity progress; implement saving of progress

--- src/main/java/com/liwanag/progress/adapters/secondary/DynamoDbProgressStore.java
+++ src/main/java/com/liwanag/progress/adapters/secondary/DynamoDbProgressStore.java
@@ -1,10 +1,12 @@
 package com.liwanag.progress.adapters.secondary;
 
-import com.liwanag.progress.adapters.secondary.persistence.entity.ProgressEntity;
-import com.liwanag.progress.adapters.secondary.persistence.entity.ProgressKeys;
+import com.liwanag.progress.adapters.secondary.persistence.entity.*;
 import com.liwanag.progress.adapters.secondary.persistence.mapper.ProgressMapper;
 import com.liwanag.progress.domain.content.FqId;
+import com.liwanag.progress.domain.progress.ActivityProgress;
+import com.liwanag.progress.domain.progress.EpisodeProgress;
 import com.liwanag.progress.domain.progress.Progress;
+import com.liwanag.progress.domain.progress.UnitProgress;
 import com.liwanag.progress.ports.secondary.ProgressStore;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -17,31 +19,71 @@ import java.util.UUID;
 @Component
 @Slf4j
 public class DynamoDbProgressStore implements ProgressStore {
-    private final DynamoDbTable<ProgressEntity> progressTable;
+    private final DynamoDbTable<UnitProgressEntity> unitProgressTable;
+    private final DynamoDbTable<EpisodeProgressEntity> episodeProgressTable;
+    private final DynamoDbTable<ActivityProgressEntity> activityProgressTable;
     private final ProgressMapper progressMapper;
 
     public DynamoDbProgressStore(
-            @Qualifier("progressTable") DynamoDbTable<ProgressEntity> progressTable,
+            @Qualifier("unitProgressTable") DynamoDbTable<UnitProgressEntity> unitProgressTable,
+            @Qualifier("episodeProgressTable") DynamoDbTable<EpisodeProgressEntity> episodeProgressTable,
+            @Qualifier("activityProgressTable") DynamoDbTable<ActivityProgressEntity> activityProgressTable,
             ProgressMapper progressMapper
     ) {
-        this.progressTable = progressTable;
+        this.unitProgressTable = unitProgressTable;
+        this.episodeProgressTable = episodeProgressTable;
+        this.activityProgressTable = activityProgressTable;
         this.progressMapper = progressMapper;
     }
 
     @Override
     public void save(Progress progress) {
-        progressTable.putItem(progressMapper.toEntity(progress));
+        switch (progress) {
+            case ActivityProgress ap -> activityProgressTable.putItem(progressMapper.toEntity(ap));
+            case EpisodeProgress ep -> episodeProgressTable.putItem(progressMapper.toEntity(ep));
+            case UnitProgress up -> unitProgressTable.putItem(progressMapper.toEntity(up));
+        }
     }
 
     @Override
-    public Optional<Progress> load(UUID userId, FqId fqid) {
-        ProgressEntity entity = progressTable.getItem(r -> r.key(k -> k
+    public Optional<ActivityProgress> loadActivity(UUID userId, FqId fqid) {
+        ActivityProgressEntity entity = activityProgressTable.getItem(r -> r.key(k -> k
                 .partitionValue(ProgressKeys.progressPk(userId))
                 .sortValue(ProgressKeys.progressSk(fqid))
         ));
 
         if (entity == null) {
-            log.error("Progress not found for userId: {} and FqId: {}", userId, fqid);
+            log.error("ActivityProgress not found for userId: {} and FqId: {}", userId, fqid);
+            return Optional.empty();
+        }
+
+        return Optional.of(progressMapper.toModel(entity));
+    }
+
+    @Override
+    public Optional<EpisodeProgress> loadEpisode(UUID userId, FqId fqid) {
+        EpisodeProgressEntity entity = episodeProgressTable.getItem(r -> r.key(k -> k
+                .partitionValue(ProgressKeys.progressPk(userId))
+                .sortValue(ProgressKeys.progressSk(fqid))
+        ));
+
+        if (entity == null) {
+            log.error("EpisodeProgress not found for userId: {} and FqId: {}", userId, fqid);
+            return Optional.empty();
+        }
+
+        return Optional.of(progressMapper.toModel(entity));
+    }
+
+    @Override
+    public Optional<UnitProgress> loadUnit(UUID userId, FqId fqid) {
+        UnitProgressEntity entity = unitProgressTable.getItem(r -> r.key(k -> k
+                .partitionValue(ProgressKeys.progressPk(userId))
+                .sortValue(ProgressKeys.progressSk(fqid))
+        ));
+
+        if (entity == null) {
+            log.error("UnitProgress not found for userId: {} and FqId: {}", userId, fqid);
             return Optional.empty();
         }
 
--- src/main/java/com/liwanag/progress/adapters/secondary/persistence/mapper/ProgressMapper.java
+++ src/main/java/com/liwanag/progress/adapters/secondary/persistence/mapper/ProgressMapper.java
@@ -12,36 +12,6 @@ import static com.liwanag.progress.adapters.secondary.persistence.entity.Progres
 
 @Component
 public final class ProgressMapper {
-//    public ProgressEntity toEntity(Progress model) {
-//        return ProgressEntity.builder()
-//                .pk(ProgressKeys.progressPk(model.getUserId()))
-//                .sk(ProgressKeys.progressSk(model.getFqid()))
-//                .userId(model.getUserId().toString())
-//                .status(model.getStatus().name())
-//                .unitId(model.getFqid().getUnitId())
-//                .episodeId(model.getFqid().getEpisodeId())
-//                .activityId(model.getFqid().getActivityId())
-//                .totalCount(model.getTotalCount())
-//                .completedCount(model.getCompletedCount())
-//                .firstStartedAt(model.getFirstStartedAt().toEpochMilli())
-//                .lastUpdatedAt(model.getLastUpdatedAt().toEpochMilli())
-//                .completedAt(model.getCompletedAt() != null ? model.getCompletedAt().toEpochMilli() : null)
-//                .build();
-//    }
-//
-//    public Progress toModel(ProgressEntity entity) {
-//        return Progress.builder()
-//                .userId(UUID.fromString(entity.getUserId()))
-//                .fqid(ProgressKeys.extractFqId(entity.getSk()))
-//                .status(ProgressStatus.valueOf(entity.getStatus())) // TODO: perform exception handling
-//                .totalCount(entity.getTotalCount())
-//                .completedCount(entity.getCompletedCount())
-//                .firstStartedAt(java.time.Instant.ofEpochMilli(entity.getFirstStartedAt()))
-//                .lastUpdatedAt(java.time.Instant.ofEpochMilli(entity.getLastUpdatedAt()))
-//                .completedAt(entity.getCompletedAt() != null ? java.time.Instant.ofEpochMilli(entity.getCompletedAt()) : null)
-//                .build();
-//    }
-
     public ActivityProgressEntity toEntity(ActivityProgress model) {
         return ActivityProgressEntity.builder()
                 .pk(progressPk(model.getUserId()))
